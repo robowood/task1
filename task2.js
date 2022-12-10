bfe5f0f6c6fd7ffc686760a56ea876a20d5b74e6
robowood <118734805+robowood@users.noreply.github.com>
2022-12-10 14:06:36 +0530

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,43 @@
+//Write the code as per the youtuber does
+//var title=(document.getElementsByClassName('title'));
+//title.style.color='green';
+//var items=document.getElementsByClassName('list-group-item');
+//console.log(items);
+//console.log(items[1]);
+//items[1].textContent='hello2';
+//items[1].style.fontWeight='bold';
+//items[1].style.backgroundColor='yellow';
+//items[2].style.backgroundColor='green';
+
+//give error
+//items.style.backgroundColor='#f4f4f4';
+
+// for(var i=0; i< items.length; i++){
+
+//     items[i].style.backgroundColor = '#f4f4f4';
+// }
+// for(var i=0;i<items.length;i++){
+
+//      items[i].style.fontWeight='bold';
+// }
+// var items1=document.getElementsByClassName('list-group-item1');
+// items1[0].style.backgroundColor='pink';
+
+
+
+
+//Make the 3 rd element in the list have green 
+
+//var items=document.getElementsByClassName('list-group-item');
+//items[2].style.backgroundColor='green';
+
+
+
+
+
+//Make all the elements in the list have bold color font
+
+//for(var i=0;i<items.length;i++){
+
+    //      items[i].style.fontWeight='bold';
+    // }
---
+++ task4.js
@@ -0,0 +1,45 @@
+//Make the 2nd item have green background color
+
+//var secondItem=document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor='green';
+
+
+
+//Make the 3rd item invisible
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Using QuerySelectorALL change the font color to green for 2nd item in the item list
+
+
+//var item=document.querySelectorAll('.list-group-item');
+//item[2].style.color='green';
+
+
+
+
+
+
+//Choose all the odd elements and make their background green using 
+
+// var odd=document.querySelectorAll('li:nth-child(odd)')
+// for (var i=0;i<odd.lenght;i+++){
+//     odd[i].style.backgroundColor='green';
+
+
+
+
+
+
+//What is the difference between queryselector and queryselectorall
